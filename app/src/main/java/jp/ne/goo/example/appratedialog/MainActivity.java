0cf5a54f0652813b7181236ff901b34b6b2f513d
ksugawara61 <katsuya@kokohadoko.net>
2018-01-31 15:56:59 +0900

    first commit

---
+++ app/src/main/java/jp/ne/goo/example/appratedialog/MainActivity.java
@@ -0,0 +1,13 @@
+package jp.ne.goo.example.appratedialog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
